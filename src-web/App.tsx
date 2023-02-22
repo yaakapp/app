e42188a62712be5ec06aabe5822065dbfa2920a5
Gregory Schier <gschier1990@gmail.com>
2023-02-22 15:58:04 -0800

    Cleaner URL bar and some improvements

--- src-web/App.tsx
+++ src-web/App.tsx
@@ -1,12 +1,12 @@
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
 import Editor from './components/Editor/Editor';
-import { Input } from './components/Input';
 import { HStack, VStack } from './components/Stacks';
-import { Button } from './components/Button';
 import { DropdownMenuRadio } from './components/Dropdown';
 import { WindowDragRegion } from './components/WindowDragRegion';
 import { IconButton } from './components/IconButton';
+import { Sidebar } from './components/Sidebar';
+import { UrlBar } from './components/UrlBar';
 
 interface Response {
   url: string;
@@ -21,24 +21,22 @@ interface Response {
 function App() {
   const [error, setError] = useState<string | null>(null);
   const [response, setResponse] = useState<Response | null>(null);
-  const [url, setUrl] = useState('https://go-server.schier.dev/debug');
-  const [loading, setLoading] = useState(false);
-  const [method, setMethod] = useState<string>('get');
+  const [url, setUrl] = useState<string>('https://go-server.schier.dev/debug');
+  const [method, setMethod] = useState<{ label: string; value: string }>({
+    label: 'GET',
+    value: 'GET',
+  });
 
-  async function sendRequest(e: FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setLoading(true);
+  async function sendRequest() {
     setError(null);
 
     try {
-      const resp = (await invoke('send_request', { method, url })) as Response;
+      const resp = (await invoke('send_request', { method: method.value, url })) as Response;
       if (resp.body.includes('<head>')) {
         resp.body = resp.body.replace(/<head>/gi, `<head><base href="${resp.url}"/>`);
       }
-      setLoading(false);
       setResponse(resp);
     } catch (err) {
-      setLoading(false);
       setError(`${err}`);
     }
   }
@@ -48,9 +46,9 @@ function App() {
   return (
     <>
       <div className="grid grid-cols-[auto_1fr] h-full">
-        <nav className="w-52 bg-gray-50 h-full border-r border-gray-500/10">
+        <Sidebar>
           <HStack as={WindowDragRegion} className="pl-24 px-1" items="center" justify="end">
-            <IconButton icon="archive" size="sm" />
+            <IconButton icon="archive" />
             <DropdownMenuRadio
               onValueChange={null}
               value={'get'}
@@ -60,51 +58,23 @@ function App() {
                 { label: 'This one is just alright', value: 'post' },
               ]}
             >
-              <IconButton icon="camera" size="sm" />
+              <IconButton icon="camera" />
             </DropdownMenuRadio>
           </HStack>
-        </nav>
+        </Sidebar>
         <VStack className="w-full">
           <HStack as={WindowDragRegion} items="center" className="pl-4 pr-1">
             <h5 className="pointer-events-none text-gray-800">Send Request</h5>
-            <IconButton icon="gear" className="ml-auto" size="sm" />
+            <IconButton icon="gear" className="ml-auto" />
           </HStack>
-          <VStack className="p-4 max-w-[35rem] mx-auto" space={3}>
-            <HStack as="form" className="items-end" onSubmit={sendRequest} space={2}>
-              <DropdownMenuRadio
-                onValueChange={setMethod}
-                value={method}
-                label="HTTP Method"
-                items={[
-                  { label: 'GET', value: 'get' },
-                  { label: 'PUT', value: 'put' },
-                  { label: 'POST', value: 'post' },
-                ]}
-              >
-                <Button disabled={loading} color="secondary" forDropdown>
-                  {method.toUpperCase()}
-                </Button>
-              </DropdownMenuRadio>
-              <HStack>
-                <Input
-                  hideLabel
-                  name="url"
-                  label="Enter URL"
-                  className="rounded-r-none font-mono"
-                  onChange={(e) => setUrl(e.currentTarget.value)}
-                  value={url}
-                  placeholder="Enter a URL..."
-                />
-                <Button
-                  className="mr-1 rounded-l-none -ml-3"
-                  color="primary"
-                  type="submit"
-                  disabled={loading}
-                >
-                  {loading ? 'Sending...' : 'Send'}
-                </Button>
-              </HStack>
-            </HStack>
+          <VStack className="p-4 max-w-[50rem] mx-auto" space={3}>
+            <UrlBar
+              method={method}
+              url={url}
+              onMethodChange={setMethod}
+              onUrlChange={setUrl}
+              sendRequest={sendRequest}
+            />
             {error && <div className="text-white bg-red-500 px-4 py-1 rounded">{error}</div>}
             {response !== null && (
               <>
--- src-web/components/IconButton.tsx
+++ src-web/components/IconButton.tsx
@@ -2,15 +2,15 @@ import { forwardRef } from 'react';
 import { Icon, IconProps } from './Icon';
 import { Button, ButtonProps } from './Button';
 
-type Props = ButtonProps & IconProps;
+type Props = Omit<IconProps, 'size'> & ButtonProps;
 
 export const IconButton = forwardRef<HTMLButtonElement, Props>(function IconButton(
-  { icon, ...props }: Props,
+  { icon, spin, ...props }: Props,
   ref,
 ) {
   return (
     <Button ref={ref} className="group" {...props}>
-      <Icon icon={icon} className="text-gray-700 group-hover:text-gray-900" />
+      <Icon icon={icon} spin={spin} className="text-gray-700 group-hover:text-gray-900" />
     </Button>
   );
 });
