a77e3787306c9d16b6cd978b2cb79582fd081e12
Gregory Schier <gschier1990@gmail.com>
2023-02-24 16:09:19 -0800

    Send request body

--- src-web/components/Editor/Editor.tsx
+++ src-web/components/Editor/Editor.tsx
@@ -4,9 +4,14 @@ import './Editor.css';
 interface Props {
   contentType: string;
   value: string;
+  onChange?: (value: string) => void;
 }
 
 export default function Editor(props: Props) {
-  const { ref } = useCodeMirror({ value: props.value, contentType: props.contentType });
+  const { ref } = useCodeMirror({
+    value: props.value,
+    contentType: props.contentType,
+    onChange: props.onChange,
+  });
   return <div ref={ref} className="cm-wrapper" />;
 }
