957739ba5e1e703199f36a4e96ad14d7e491cc7b
Gregory Schier <gschier1990@gmail.com>
2023-03-02 11:14:51 -0800

    Improved autocompletion!

--- src-web/components/Editor/twig/extension.ts
+++ src-web/components/Editor/twig/extension.ts
@@ -1,18 +1,18 @@
 import { LanguageSupport, LRLanguage } from '@codemirror/language';
 import { parseMixed } from '@lezer/common';
-import { myCompletions } from '../completion/completion';
+import { completions } from './completion';
 import { placeholders } from '../widgets';
 import { parser as twigParser } from './twig';
 
 export function twig(base?: LanguageSupport) {
   const language = mixedOrPlainLanguage(base);
   const completion = language.data.of({
-    autocomplete: myCompletions,
+    autocomplete: completions,
   });
   const languageSupport = new LanguageSupport(language, [completion]);
 
   if (base) {
-    const completion2 = base.language.data.of({ autocomplete: myCompletions });
+    const completion2 = base.language.data.of({ autocomplete: completions });
     const languageSupport2 = new LanguageSupport(base.language, [completion2]);
     return [languageSupport, languageSupport2, placeholders, base.support];
   } else {
