aaf95f565f6263f80470578e2bbe5a5e73ba1e5b
Gregory Schier <gschier1990@gmail.com>
2023-03-02 17:56:53 -0800

    More colors

--- src-web/components/Editor/url/highlight.ts
+++ src-web/components/Editor/url/highlight.ts
@@ -2,8 +2,8 @@ import { styleTags, tags as t } from '@lezer/highlight';
 
 export const highlight = styleTags({
   Protocol: t.comment,
-  Port: t.attributeName,
-  Host: t.variableName,
-  Path: t.bool,
-  Query: t.string,
+  // Port: t.attributeName,
+  // Host: t.variableName,
+  // Path: t.bool,
+  // Query: t.string,
 });
