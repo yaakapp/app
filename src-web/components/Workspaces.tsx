03e36e7f1c282454b1a30e6ad64b009d739a4994
Gregory Schier <gschier1990@gmail.com>
2023-03-15 23:33:46 -0700

    Some fixes

--- src-web/components/Workspaces.tsx
+++ src-web/components/Workspaces.tsx
@@ -6,7 +6,7 @@ import { VStack } from './core/Stacks';
 export default function Workspaces() {
   const workspaces = useWorkspaces();
   return (
-    <VStack as="ul" className="p-12">
+    <VStack as="ul" className="p-12" space={1}>
       <Heading>Workspaces</Heading>
       {workspaces.map((w) => (
         <Button key={w.id} color="gray" to={`/workspaces/${w.id}`}>
