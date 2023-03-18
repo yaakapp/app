3a7f0898f918491565cea81cfdd3825bbdd7a21c
Gregory Schier <gschier1990@gmail.com>
2023-03-17 17:57:43 -0700

    Fix mixed parser

--- src-web/components/core/Editor/text/extension.ts
+++ src-web/components/core/Editor/text/extension.ts
@@ -2,6 +2,7 @@ import { LanguageSupport, LRLanguage } from '@codemirror/language';
 import { parser } from './text';
 
 const textLanguage = LRLanguage.define({
+  name: 'text',
   parser,
   languageData: {},
 });
