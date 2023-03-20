8306bc2198ce4fb481fa1334d1085e977f201512
Gregory Schier <gschier1990@gmail.com>
2023-03-20 00:17:29 -0700

    Pair validation

--- src-web/components/UrlBar.tsx
+++ src-web/components/UrlBar.tsx
@@ -1,6 +1,6 @@
 import classnames from 'classnames';
 import type { FormEvent } from 'react';
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useCallback } from 'react';
 import { useIsResponseLoading } from '../hooks/useIsResponseLoading';
 import { useSendRequest } from '../hooks/useSendRequest';
 import { useUpdateRequest } from '../hooks/useUpdateRequest';
@@ -20,7 +20,6 @@ export const UrlBar = memo(function UrlBar({ request, className }: Props) {
   const handleMethodChange = useCallback((method: string) => updateRequest.mutate({ method }), []);
   const handleUrlChange = useCallback((url: string) => updateRequest.mutate({ url }), []);
   const loading = useIsResponseLoading(request.id);
-  const useEditor = useMemo(() => ({ useTemplating: true, contentType: 'url' }), []);
 
   const handleSubmit = useCallback(
     async (e: FormEvent) => {
@@ -35,7 +34,8 @@ export const UrlBar = memo(function UrlBar({ request, className }: Props) {
       <Input
         key={request.id}
         hideLabel
-        useEditor={useEditor}
+        useTemplating
+        contentType="url"
         className="px-0"
         name="url"
         label="Enter URL"
