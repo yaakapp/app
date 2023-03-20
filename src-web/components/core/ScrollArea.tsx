fa222bdf12b917578664f99365c1fbb298b9f56f
Gregory Schier <gschier1990@gmail.com>
2023-03-20 01:18:44 -0700

    Fix pair editor container

--- src-web/components/core/ScrollArea.tsx
+++ src-web/components/core/ScrollArea.tsx
@@ -5,11 +5,15 @@ import type { ReactNode } from 'react';
 interface Props {
   children: ReactNode;
   className?: string;
+  type?: S.ScrollAreaProps['type'];
 }
 
-export function ScrollArea({ children, className }: Props) {
+export function ScrollArea({ children, className, type }: Props) {
   return (
-    <S.Root className={classnames(className, 'group/scroll overflow-hidden')} type="hover">
+    <S.Root
+      className={classnames(className, 'group/scroll overflow-hidden')}
+      type={type ?? 'hover'}
+    >
       <S.Viewport className="h-full w-full">{children}</S.Viewport>
       <ScrollBar orientation="vertical" />
       <ScrollBar orientation="horizontal" />
