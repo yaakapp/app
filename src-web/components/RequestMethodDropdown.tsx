5a3b47846a2780bf2b99a555af8d44ba102c6ba3
Gregory Schier <gschier1990@gmail.com>
2023-03-20 17:01:29 -0700

    Fix URL bar spacing

--- src-web/components/RequestMethodDropdown.tsx
+++ src-web/components/RequestMethodDropdown.tsx
@@ -4,6 +4,7 @@ import { RadioDropdown } from './core/RadioDropdown';
 
 type Props = {
   method: string;
+  className?: string;
   onChange: (method: string) => void;
 };
 
@@ -15,10 +16,11 @@ const methodItems = ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD']
 export const RequestMethodDropdown = memo(function RequestMethodDropdown({
   method,
   onChange,
+  className,
 }: Props) {
   return (
     <RadioDropdown value={method} items={methodItems} onChange={onChange}>
-      <Button type="button" size="sm" className="mx-0.5" justify="start">
+      <Button size="sm" className={className}>
         {method.toUpperCase()}
       </Button>
     </RadioDropdown>
