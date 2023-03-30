7c2611a5a78368179c49d288b38e0d44fa04bef8
Gregory Schier <gschier1990@gmail.com>
2023-03-30 16:49:49 -0700

    Dedicated event for model creation

--- src-web/components/SidebarDisplayToggle.tsx
+++ src-web/components/SidebarDisplayToggle.tsx
@@ -1,16 +1,32 @@
-import { memo } from 'react';
+import { memo, useCallback } from 'react';
+import { useCreateRequest } from '../hooks/useCreateRequest';
 import { useSidebarDisplay } from '../hooks/useSidebarDisplay';
 import { IconButton } from './core/IconButton';
+import { HStack } from './core/Stacks';
 
 export const SidebarDisplayToggle = memo(function SidebarDisplayToggle() {
   const sidebarDisplay = useSidebarDisplay();
+  const createRequest = useCreateRequest({ navigateAfter: true });
+  const handleCreateRequest = useCallback(() => {
+    createRequest.mutate({ name: 'New Request' });
+  }, []);
+
   return (
-    <IconButton
-      onClick={sidebarDisplay.toggle}
-      className="pointer-events-auto"
-      size="sm"
-      title="Show sidebar"
-      icon={sidebarDisplay.hidden ? 'leftPanelHidden' : 'leftPanelVisible'}
-    />
+    <HStack space={1}>
+      <IconButton
+        onClick={sidebarDisplay.toggle}
+        className="pointer-events-auto"
+        size="sm"
+        title="Show sidebar"
+        icon={sidebarDisplay.hidden ? 'leftPanelHidden' : 'leftPanelVisible'}
+      />
+      <IconButton
+        onClick={handleCreateRequest}
+        className="pointer-events-auto"
+        size="sm"
+        title="Show sidebar"
+        icon="plusCircle"
+      />
+    </HStack>
   );
 });
