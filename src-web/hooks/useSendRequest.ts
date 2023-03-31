6dc3eecca4d34531910ee2ac5f15627111466d06
Gregory Schier <gschier1990@gmail.com>
2023-03-31 16:14:25 -0700

    Tweak

--- src-web/hooks/useSendRequest.ts
+++ src-web/hooks/useSendRequest.ts
@@ -1,19 +1,9 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api';
 import type { HttpResponse } from '../lib/models';
-import { responsesQueryKey } from './useResponses';
 
 export function useSendRequest(id: string | null) {
-  const queryClient = useQueryClient();
   return useMutation<HttpResponse, string>({
-    mutationFn: async () => {
-      return invoke('send_request', { requestId: id });
-    },
-    onSuccess: (response) => {
-      queryClient.setQueryData<HttpResponse[]>(responsesQueryKey(response), (responses) => [
-        ...(responses ?? []),
-        response,
-      ]);
-    },
+    mutationFn: () => invoke('send_request', { requestId: id }),
   }).mutate;
 }
