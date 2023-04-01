ebc9939498e5e91f790f7c950db9104453c194fc
Gregory Schier <gschier1990@gmail.com>
2023-03-31 22:54:32 -0700

    Fix request duplication

--- src-web/hooks/useDuplicateRequest.ts
+++ src-web/hooks/useDuplicateRequest.ts
@@ -1,6 +1,8 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api';
+import type { HttpRequest } from '../lib/models';
 import { useActiveWorkspaceId } from './useActiveWorkspaceId';
+import { requestsQueryKey } from './useRequests';
 import { useRoutes } from './useRoutes';
 
 export function useDuplicateRequest({
@@ -12,14 +14,19 @@ export function useDuplicateRequest({
 }) {
   const workspaceId = useActiveWorkspaceId();
   const routes = useRoutes();
-  return useMutation<string, string>({
+  const queryClient = useQueryClient();
+  return useMutation<HttpRequest, string>({
     mutationFn: async () => {
       if (id === null) throw new Error("Can't duplicate a null request");
       return invoke('duplicate_request', { id });
     },
-    onSuccess: async (newId: string) => {
+    onSuccess: async (request) => {
+      queryClient.setQueryData<HttpRequest[]>(
+        requestsQueryKey({ workspaceId: request.workspaceId }),
+        (requests) => [...(requests ?? []), request],
+      );
       if (navigateAfter && workspaceId !== null) {
-        routes.navigate('request', { workspaceId, requestId: newId });
+        routes.navigate('request', { workspaceId, requestId: request.id });
       }
     },
   });
