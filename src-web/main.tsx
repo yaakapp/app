197bc38b6916b81ea3578b95be4a3c6b17f7342a
Gregory Schier <gschier1990@gmail.com>
2023-04-04 16:23:08 -0700

    remove janky last location tracking

--- src-web/main.tsx
+++ src-web/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import { getKeyValue } from './lib/keyValueStore';
-import { syncLastLocation } from './lib/lastLocation';
 import { getPreferredAppearance, setAppearance } from './lib/theme/window';
 import './main.css';
 
 setAppearance(await getKeyValue({ key: 'appearance', fallback: getPreferredAppearance() }));
-await syncLastLocation();
 
 // root holds our app's root DOM Element:
 createRoot(document.getElementById('root') as HTMLElement).render(
