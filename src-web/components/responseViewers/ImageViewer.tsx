577cfe5bdced11ac82040baaf7b7788623415d13
Gregory Schier <gschier1990@gmail.com>
2023-04-13 22:33:47 -0700

    Fix imageview padding

--- src-web/components/responseViewers/ImageViewer.tsx
+++ src-web/components/responseViewers/ImageViewer.tsx
@@ -1,15 +1,23 @@
 import { convertFileSrc } from '@tauri-apps/api/tauri';
+import classnames from 'classnames';
 import type { HttpResponse } from '../../lib/models';
 
 interface Props {
   response: HttpResponse;
+  className?: string;
 }
 
-export function ImageViewer({ response }: Props) {
+export function ImageViewer({ response, className }: Props) {
   if (response.bodyPath === null) {
     return <div>Empty response body</div>;
   }
 
   const src = convertFileSrc(response.bodyPath);
-  return <img src={src} alt="Response preview" />;
+  return (
+    <img
+      src={src}
+      alt="Response preview"
+      className={classnames(className, 'max-w-full max-h-full')}
+    />
+  );
 }
