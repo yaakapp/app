13155f859154536da8d766d9ea637474ace375c1
Gregory Schier <gschier1990@gmail.com>
2023-10-29 12:05:05 -0700

    Fix request creation

--- src-web/components/SidebarActions.tsx
+++ src-web/components/SidebarActions.tsx
@@ -2,28 +2,29 @@ import { memo } from 'react';
 import { useSidebarHidden } from '../hooks/useSidebarHidden';
 import { IconButton } from './core/IconButton';
 import { useCreateRequest } from '../hooks/useCreateRequest';
+import { HStack } from './core/Stacks';
 
 export const SidebarActions = memo(function SidebarActions() {
   const createRequest = useCreateRequest();
   const { hidden, toggle } = useSidebarHidden();
 
-  if (!hidden) return null;
-
   return (
-  <div>
+    <HStack>
+      {hidden && (
         <IconButton
+          onClick={toggle}
+          className="pointer-events-auto"
           size="sm"
-          icon="plusCircle"
-          title="Create Request"
-          onClick={() => createRequest.mutate({})}
+          title="Show sidebar"
+          icon={hidden ? 'leftPanelHidden' : 'leftPanelVisible'}
         />
-    <IconButton
-      onClick={toggle}
-      className="pointer-events-auto"
-      size="sm"
-      title="Show sidebar"
-      icon={hidden ? 'leftPanelHidden' : 'leftPanelVisible'}
-    />
-    </div>
+      )}
+      <IconButton
+        size="sm"
+        icon="plusCircle"
+        title="Create Request"
+        onClick={() => createRequest.mutate({})}
+      />
+    </HStack>
   );
 });
