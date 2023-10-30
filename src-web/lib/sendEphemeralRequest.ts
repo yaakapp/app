32b135dbaf53b0f5a8ff0e1751e6b9a00d6fc71d
Gregory Schier <gschier1990@gmail.com>
2023-10-30 08:24:49 -0700

    Fix sending of ephemeral requests

--- src-web/lib/sendEphemeralRequest.ts
+++ src-web/lib/sendEphemeralRequest.ts
@@ -1,8 +1,8 @@
 import { invoke } from '@tauri-apps/api';
 import type { HttpRequest, HttpResponse } from './models';
 
-export async function sendEphemeralRequest(request: HttpRequest): Promise<HttpResponse> {
+export async function sendEphemeralRequest(request: HttpRequest, environmentId: string | null): Promise<HttpResponse> {
   // Remove some things that we don't want to associate
   const newRequest = { ...request, id: '', requestId: '', workspaceId: '' };
-  return invoke('send_ephemeral_request', { request: newRequest });
+  return invoke('send_ephemeral_request', { request: newRequest, environmentId });
 }
