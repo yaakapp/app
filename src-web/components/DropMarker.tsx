11166470aa808f458f832aef6eecff08f1649794
Gregory Schier <gschier1990@gmail.com>
2023-11-04 11:16:41 -0700

    Fix drop marker indent

--- src-web/components/DropMarker.tsx
+++ src-web/components/DropMarker.tsx
@@ -3,16 +3,14 @@ import React, { memo } from 'react';
 
 interface Props {
   className?: string;
-  depth: number;
 }
 
 export const DropMarker = memo(
-  function DropMarker({ className, depth }: Props) {
+  function DropMarker({ className }: Props) {
     return (
       <div
         className={classNames(
           className,
-          depth > 0 ? 'ml-5' : 'ml-0',
           'relative w-full h-0 overflow-visible pointer-events-none',
         )}
       >
