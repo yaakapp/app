10f3722fe35c941b19bfbf2269725b836184b2f5
Gregory Schier <gschier1990@gmail.com>
2023-11-09 17:34:57 -0800

    Request fallback name in header

---
+++ src-web/lib/fallbackRequestName.ts
@@ -0,0 +1,25 @@
+import type { HttpRequest } from './models';
+
+export function fallbackRequestName(r: HttpRequest | null): string {
+  if (r == null) return '';
+
+  if (r.name) {
+    return r.name;
+  }
+
+  if (r.url.trim() === '') {
+    return 'New Request';
+  }
+
+  const fixedUrl = r.url.match(/^https?:\/\//) ? r.url : 'http://' + r.url;
+
+  try {
+    const url = new URL(fixedUrl);
+    const pathname = url.pathname === '/' ? '' : url.pathname;
+    return `${url.host}${pathname}`;
+  } catch (_) {
+    // Nothing
+  }
+
+  return '';
+}
