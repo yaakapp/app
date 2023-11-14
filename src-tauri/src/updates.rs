da414debe1e09f01e4efc76de9670cf6b66d43e0
Gregory Schier <gschier1990@gmail.com>
2023-11-14 08:57:46 -0800

    Beta channel updates

--- src-tauri/src/updates.rs
+++ src-tauri/src/updates.rs
@@ -1,5 +1,6 @@
 use std::time::SystemTime;
 
+use log::info;
 use tauri::{AppHandle, updater, Window, Wry};
 use tauri::api::dialog;
 
@@ -11,19 +12,31 @@ pub struct YaakUpdater {
     last_update_check: SystemTime,
 }
 
+pub enum UpdateMode {
+    Stable,
+    Beta,
+}
+
 impl YaakUpdater {
     pub fn new() -> Self {
         Self {
             last_update_check: SystemTime::UNIX_EPOCH,
         }
     }
-    pub async fn check(&mut self, app_handle: &AppHandle<Wry>) -> Result<(), updater::Error> {
-        if self.last_update_check.elapsed().unwrap().as_secs() < MAX_UPDATE_CHECK_SECONDS {
-            return Ok(());
-        }
-
+    pub async fn force_check(
+        &mut self,
+        app_handle: &AppHandle<Wry>,
+        mode: UpdateMode,
+    ) -> Result<(), updater::Error> {
         self.last_update_check = SystemTime::now();
-        match app_handle.updater().check().await {
+        let update_mode = get_update_mode_str(mode);
+        info!("Checking for updates mode={}", update_mode);
+        match app_handle
+            .updater()
+            .header("X-Update-Mode", update_mode)?
+            .check()
+            .await
+        {
             Ok(update) => {
                 if dialog::blocking::ask(
                     None::<&Window>,
@@ -37,5 +50,30 @@ impl YaakUpdater {
             Err(updater::Error::UpToDate) => Ok(()),
             Err(e) => Err(e),
         }
+    }
+    pub async fn check(
+        &mut self,
+        app_handle: &AppHandle<Wry>,
+        mode: UpdateMode,
+    ) -> Result<(), updater::Error> {
+        if self.last_update_check.elapsed().unwrap().as_secs() < MAX_UPDATE_CHECK_SECONDS {
+            return Ok(());
+        }
+
+        self.force_check(app_handle, mode).await
+    }
+}
+
+pub fn update_mode_from_str(mode: &str) -> UpdateMode {
+    match mode {
+        "beta" => UpdateMode::Beta,
+        _ => UpdateMode::Stable,
+    }
+}
+
+fn get_update_mode_str(mode: UpdateMode) -> &'static str {
+    match mode {
+        UpdateMode::Stable => "stable",
+        UpdateMode::Beta => "beta",
     }
 }
