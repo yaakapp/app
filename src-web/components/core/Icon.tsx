466d412e6586635f540f9748ac8b852043f73397
Gregory Schier <gschier1990@gmail.com>
2024-01-17 18:48:43 -0800

    Workspace header tweak Windows

--- src-web/components/core/Icon.tsx
+++ src-web/components/core/Icon.tsx
@@ -1,66 +1,63 @@
 import * as lucide from 'lucide-react';
 import classNames from 'classnames';
-import type { HTMLAttributes } from 'react';
-import { memo } from 'react';
-import { ReactComponent as LeftPanelHiddenIcon } from '../../assets/icons/LeftPanelHiddenIcon.svg';
-import { ReactComponent as LeftPanelVisibleIcon } from '../../assets/icons/LeftPanelVisibleIcon.svg';
+import type {HTMLAttributes} from 'react';
+import {memo} from 'react';
 
 const icons = {
-  archive: lucide.ArchiveIcon,
-  box: lucide.BoxIcon,
-  chat: lucide.MessageSquare,
-  check: lucide.CheckIcon,
-  chevronDown: lucide.ChevronDownIcon,
-  chevronRight: lucide.ChevronRightIcon,
-  code: lucide.CodeIcon,
-  copy: lucide.CopyIcon,
-  moreVertical: lucide.MoreVerticalIcon,
-  download: lucide.DownloadIcon,
-  gripVertical: lucide.GripVerticalIcon,
-  eye: lucide.EyeIcon,
-  eyeClosed: lucide.EyeOffIcon,
-  filter: lucide.FilterIcon,
-  flask: lucide.FlaskConicalIcon,
-  keyboard: lucide.KeyboardIcon,
-  magicWand: lucide.Wand2Icon,
-  externalLink: lucide.ExternalLinkIcon,
-  sendHorizontal: lucide.SendHorizonalIcon,
-  pencil: lucide.PencilIcon,
-  question: lucide.ShieldQuestionIcon,
-  plus: lucide.PlusIcon,
-  plusCircle: lucide.PlusCircleIcon,
-  settings: lucide.SettingsIcon,
-  settings2: lucide.Settings2Icon,
-  trash: lucide.TrashIcon,
-  update: lucide.RefreshCcwIcon,
-  upload: lucide.UploadIcon,
-  x: lucide.XIcon,
+    archive: lucide.ArchiveIcon,
+    box: lucide.BoxIcon,
+    chat: lucide.MessageSquare,
+    check: lucide.CheckIcon,
+    chevronDown: lucide.ChevronDownIcon,
+    chevronRight: lucide.ChevronRightIcon,
+    code: lucide.CodeIcon,
+    copy: lucide.CopyIcon,
+    download: lucide.DownloadIcon,
+    externalLink: lucide.ExternalLinkIcon,
+    eye: lucide.EyeIcon,
+    eyeClosed: lucide.EyeOffIcon,
+    filter: lucide.FilterIcon,
+    flask: lucide.FlaskConicalIcon,
+    gripVertical: lucide.GripVerticalIcon,
+    keyboard: lucide.KeyboardIcon,
+    leftPanelHidden: lucide.PanelLeftOpenIcon,
+    leftPanelVisible: lucide.PanelLeftCloseIcon,
+    magicWand: lucide.Wand2Icon,
+    moreVertical: lucide.MoreVerticalIcon,
+    pencil: lucide.PencilIcon,
+    plus: lucide.PlusIcon,
+    plusCircle: lucide.PlusCircleIcon,
+    question: lucide.ShieldQuestionIcon,
+    sendHorizontal: lucide.SendHorizonalIcon,
+    settings2: lucide.Settings2Icon,
+    settings: lucide.SettingsIcon,
+    trash: lucide.TrashIcon,
+    update: lucide.RefreshCcwIcon,
+    upload: lucide.UploadIcon,
+    x: lucide.XIcon,
 
-  // Custom
-  leftPanelHidden: LeftPanelHiddenIcon,
-  leftPanelVisible: LeftPanelVisibleIcon,
-  empty: (props: HTMLAttributes<HTMLSpanElement>) => <span {...props} />,
+    empty: (props: HTMLAttributes<HTMLSpanElement>) => <span {...props} />,
 };
 
 export interface IconProps {
-  icon: keyof typeof icons;
-  className?: string;
-  size?: 'xs' | 'sm' | 'md';
-  spin?: boolean;
+    icon: keyof typeof icons;
+    className?: string;
+    size?: 'xs' | 'sm' | 'md';
+    spin?: boolean;
 }
 
-export const Icon = memo(function Icon({ icon, spin, size = 'md', className }: IconProps) {
-  const Component = icons[icon] ?? icons.question;
-  return (
-    <Component
-      className={classNames(
-        className,
-        'text-inherit',
-        size === 'md' && 'h-4 w-4',
-        size === 'sm' && 'h-3.5 w-3.5',
-        size === 'xs' && 'h-3 w-3',
-        spin && 'animate-spin',
-      )}
-    />
-  );
+export const Icon = memo(function Icon({icon, spin, size = 'md', className}: IconProps) {
+    const Component = icons[icon] ?? icons.question;
+    return (
+        <Component
+            className={classNames(
+                className,
+                'text-inherit',
+                size === 'md' && 'h-4 w-4',
+                size === 'sm' && 'h-3.5 w-3.5',
+                size === 'xs' && 'h-3 w-3',
+                spin && 'animate-spin',
+            )}
+        />
+    );
 });
