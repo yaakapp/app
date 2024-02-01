a05fc5fd20d5108f0cf9b5ec002fdaee26956432
Gregory Schier <gschier1990@gmail.com>
2024-01-31 22:13:46 -0800

    Hacky server streaming done

--- src-web/hooks/useAlert.ts
+++ src-web/hooks/useAlert.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import type { DialogProps } from '../components/core/Dialog';
 import { useDialog } from '../components/DialogContext';
 import type { AlertProps } from './Alert';
@@ -5,20 +6,16 @@ import { Alert } from './Alert';
 
 export function useAlert() {
   const dialog = useDialog();
-  return ({
-    id,
-    title,
-    body,
-  }: {
-    id: string;
-    title: DialogProps['title'];
-    body: AlertProps['body'];
-  }) =>
-    dialog.show({
-      id,
-      title,
-      hideX: true,
-      size: 'sm',
-      render: ({ hide }) => Alert({ onHide: hide, body }),
-    });
+  return useCallback(
+    ({ id, title, body }: { id: string; title: DialogProps['title']; body: AlertProps['body'] }) =>
+      dialog.show({
+        id,
+        title,
+        hideX: true,
+        size: 'sm',
+        render: ({ hide }) => Alert({ onHide: hide, body }),
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
 }
