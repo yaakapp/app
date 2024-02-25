06177c80771e75d9420618fec64f90747fddc38d
Gregory Schier <gschier1990@gmail.com>
2024-02-24 18:19:16 -0800

    Add pkg locks

--- src-web/lib/data/headerNames.ts
+++ src-web/lib/data/headerNames.ts
@@ -1,12 +1,47 @@
-export const headerNames = [
-  'Content-Type',
-  'Content-Length',
-  'Accept',
-  'Accept-Charset',
-  'Accept-Encoding',
-  'Accept-Language',
-  'Accept-Datetime',
-  'Authorization',
+import type { GenericCompletionOption } from '../../components/core/Editor/genericCompletion';
+
+export const headerNames: (GenericCompletionOption | string)[] = [
+  {
+    type: 'constant',
+    label: 'Content-Type',
+    info: 'The original media type of the resource (prior to any content encoding applied for sending)',
+  },
+  {
+    type: 'constant',
+    label: 'Content-Length',
+    info: 'The size of the message body, in bytes, sent to the recipient',
+  },
+  {
+    type: 'constant',
+    label: 'Accept',
+    info:
+      'The content types, expressed as MIME types, the client is able to understand. ' +
+      'The server uses content negotiation to select one of the proposals and informs ' +
+      'the client of the choice with the Content-Type response header. Browsers set required ' +
+      'values for this header based on the context of the request. For example, a browser uses ' +
+      'different values in a request when fetching a CSS stylesheet, image, video, or a script.',
+  },
+  {
+    type: 'constant',
+    label: 'Accept-Encoding',
+    info:
+      'The content encoding (usually a compression algorithm) that the client can understand. ' +
+      'The server uses content negotiation to select one of the proposals and informs the client ' +
+      'of that choice with the Content-Encoding response header.',
+  },
+  {
+    type: 'constant',
+    label: 'Accept-Language',
+    info:
+      'The natural language and locale that the client prefers. The server uses content ' +
+      'negotiation to select one of the proposals and informs the client of the choice with ' +
+      'the Content-Language response header.',
+  },
+  {
+    type: 'constant',
+    label: 'Authorization',
+    info: 'Provide credentials that authenticate a user agent with a server, allowing access to a protected resource.',
+  },
   'Cache-Control',
   'Cookie',
   'Connection',
