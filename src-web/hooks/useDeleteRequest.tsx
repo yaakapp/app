35593334613d9fb84af3b87d78c2a04d9128d1d0
Gregory Schier <gschier1990@gmail.com>
2024-02-25 12:56:57 -0800

    Fix deletion in sidebar

--- src-web/hooks/useDeleteRequest.tsx
+++ src-web/hooks/useDeleteRequest.tsx
@@ -1,11 +1,21 @@
 import { useMutation } from '@tanstack/react-query';
-import type { HttpRequest } from '../lib/models';
+import type { GrpcRequest, HttpRequest } from '../lib/models';
+import { useDeleteAnyGrpcRequest } from './useDeleteAnyGrpcRequest';
 import { useDeleteAnyHttpRequest } from './useDeleteAnyHttpRequest';
 
-export function useDeleteRequest(id: string | null) {
-  const deleteAnyRequest = useDeleteAnyHttpRequest();
+export function useDeleteRequest(request: HttpRequest | GrpcRequest | null) {
+  const deleteAnyHttpRequest = useDeleteAnyHttpRequest();
+  const deleteAnyGrpcRequest = useDeleteAnyGrpcRequest();
 
-  return useMutation<HttpRequest | null, string>({
-    mutationFn: () => deleteAnyRequest.mutateAsync(id ?? 'n/a'),
+  return useMutation<void, string>({
+    mutationFn: async () => {
+      if (request?.model === 'http_request') {
+        await deleteAnyHttpRequest.mutateAsync(request.id);
+      } else if (request?.model === 'grpc_request') {
+        await deleteAnyGrpcRequest.mutateAsync(request.id);
+      } else {
+        // Request is null
+      }
+    },
   });
 }
