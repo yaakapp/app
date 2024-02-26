f034cda7cd2027ff085ec17b236243b7cccec30f
Gregory Schier <gschier1990@gmail.com>
2024-02-26 07:39:53 -0800

    Autocomplete icons and transfer proto files on duplicate

--- src-web/hooks/useDuplicateGrpcRequest.ts
+++ src-web/hooks/useDuplicateGrpcRequest.ts
@@ -1,10 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { invoke } from '@tauri-apps/api';
 import { trackEvent } from '../lib/analytics';
+import { setKeyValue } from '../lib/keyValueStore';
 import type { GrpcRequest } from '../lib/models';
 import { useActiveEnvironmentId } from './useActiveEnvironmentId';
 import { useActiveWorkspaceId } from './useActiveWorkspaceId';
 import { useAppRoutes } from './useAppRoutes';
+import { protoFilesArgs, useGrpcProtoFiles } from './useGrpcProtoFiles';
 
 export function useDuplicateGrpcRequest({
   id,
@@ -16,6 +18,7 @@ export function useDuplicateGrpcRequest({
   const activeWorkspaceId = useActiveWorkspaceId();
   const activeEnvironmentId = useActiveEnvironmentId();
   const routes = useAppRoutes();
+  const protoFiles = useGrpcProtoFiles(id);
   return useMutation<GrpcRequest, string>({
     mutationFn: async () => {
       if (id === null) throw new Error("Can't duplicate a null grpc request");
@@ -23,6 +26,9 @@ export function useDuplicateGrpcRequest({
     },
     onSettled: () => trackEvent('grpc_request', 'duplicate'),
     onSuccess: async (request) => {
+      // Also copy proto files to new request
+      await setKeyValue({ ...protoFilesArgs(request.id), value: protoFiles.value ?? [] });
+
       if (navigateAfter && activeWorkspaceId !== null) {
         routes.navigate('request', {
           workspaceId: activeWorkspaceId,
