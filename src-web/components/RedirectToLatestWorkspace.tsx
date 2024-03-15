1051f84bbf0dacd21b7fac1304ab8ca5b2e14f5d
Gregory Schier <gschier1990@gmail.com>
2024-03-15 13:07:02 -0700

    Fix deleting workspace staying on deleted workspace path

--- src-web/components/RedirectToLatestWorkspace.tsx
+++ src-web/components/RedirectToLatestWorkspace.tsx
@@ -3,17 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { useAppRoutes } from '../hooks/useAppRoutes';
 import { getRecentEnvironments } from '../hooks/useRecentEnvironments';
 import { getRecentRequests } from '../hooks/useRecentRequests';
-import { getRecentWorkspaces } from '../hooks/useRecentWorkspaces';
+import { useRecentWorkspaces } from '../hooks/useRecentWorkspaces';
 import { useWorkspaces } from '../hooks/useWorkspaces';
 
 export function RedirectToLatestWorkspace() {
   const navigate = useNavigate();
   const routes = useAppRoutes();
   const workspaces = useWorkspaces();
+  const recentWorkspaces = useRecentWorkspaces();
 
   useEffect(() => {
     (async function () {
-      const workspaceId = (await getRecentWorkspaces())[0] ?? workspaces[0]?.id ?? 'n/a';
+      const workspaceId = recentWorkspaces[0] ?? workspaces[0]?.id ?? 'n/a';
       const environmentId = (await getRecentEnvironments(workspaceId))[0];
       const requestId = (await getRecentRequests(workspaceId))[0];
 
