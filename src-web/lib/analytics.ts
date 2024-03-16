a8c10f9601797faa2b8b1a976f1f98220dc547ea
Gregory Schier <gschier1990@gmail.com>
2024-03-16 09:46:11 -0700

    Stubbed out global commands helper

--- src-web/lib/analytics.ts
+++ src-web/lib/analytics.ts
@@ -1,35 +1,38 @@
 import { invoke } from '@tauri-apps/api';
 
-export function trackEvent(
-  resource:
-    | 'app'
-    | 'cookie_jar'
-    | 'dialog'
-    | 'environment'
-    | 'folder'
-    | 'grpc_connection'
-    | 'grpc_event'
-    | 'grpc_request'
-    | 'http_request'
-    | 'http_response'
-    | 'key_value'
-    | 'setting'
-    | 'sidebar'
-    | 'workspace',
-  action:
-    | 'cancel'
-    | 'commit'
-    | 'create'
-    | 'delete'
-    | 'delete_many'
-    | 'duplicate'
-    | 'hide'
-    | 'launch'
-    | 'send'
-    | 'show'
-    | 'toggle'
-    | 'update',
+export type TrackResource =
+  | 'app'
+  | 'cookie_jar'
+  | 'dialog'
+  | 'environment'
+  | 'folder'
+  | 'grpc_connection'
+  | 'grpc_event'
+  | 'grpc_request'
+  | 'http_request'
+  | 'http_response'
+  | 'key_value'
+  | 'setting'
+  | 'sidebar'
+  | 'workspace';
 
+export type TrackAction =
+  | 'cancel'
+  | 'commit'
+  | 'create'
+  | 'delete'
+  | 'delete_many'
+  | 'duplicate'
+  | 'hide'
+  | 'launch'
+  | 'send'
+  | 'show'
+  | 'toggle'
+  | 'update';
+
+export function trackEvent(
+  resource: TrackResource,
+  action: TrackAction,
   attributes: Record<string, string | number> = {},
 ) {
   invoke('cmd_track_event', {
