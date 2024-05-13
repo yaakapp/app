80ea4c14a44898d4aa88921c0088e08c72630a1c
Gregory Schier <gschier1990@gmail.com>
2024-05-13 07:19:26 -0700

    Hide large GRPC messages by default

--- src-web/hooks/useStateWithDeps.ts
+++ src-web/hooks/useStateWithDeps.ts
@@ -1,12 +1,14 @@
+import type { DependencyList } from 'react';
 import { useEffect, useState } from 'react';
 
 /**
  * Like useState, except it will update the value when the default value changes
  */
-export function useStateSyncDefault<T>(defaultValue: T) {
+export function useStateWithDeps<T>(defaultValue: T, deps: DependencyList) {
   const [value, setValue] = useState(defaultValue);
   useEffect(() => {
     setValue(defaultValue);
-  }, [defaultValue]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
   return [value, setValue] as const;
 }
