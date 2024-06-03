a0b08614f0bfb87c3d7b029f5aa374ee8bb7538b
Gregory Schier <gschier1990@gmail.com>
2024-06-03 13:49:51 -0700

    Preserve JSON/XPath filter (Closes #22)

--- src-web/components/core/FormattedError.tsx
+++ src-web/components/core/FormattedError.tsx
@@ -9,7 +9,7 @@ export function FormattedError({ children }: Props) {
   return (
     <pre
       className={classNames(
-        'w-full select-auto cursor-text bg-gray-100 p-3 rounded',
+        'w-full select-auto cursor-text bg-background-highlight-secondary p-3 rounded',
         'whitespace-pre-wrap border border-fg-danger border-dashed overflow-x-auto',
       )}
     >
