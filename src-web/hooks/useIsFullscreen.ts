b39335dc4fd25b2d1a66e04dac72a7067c5e55b9
Gregory Schier <gschier1990@gmail.com>
2024-06-02 23:38:56 -0700

    Don't call is_fullscreen so much

--- src-web/hooks/useIsFullscreen.ts
+++ src-web/hooks/useIsFullscreen.ts
@@ -1,26 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
 import { getCurrent } from '@tauri-apps/api/webviewWindow';
-import { useEffect, useState } from 'react';
 import { useWindowSize } from 'react-use';
+import { useDebouncedValue } from './useDebouncedValue';
 
 export function useIsFullscreen() {
-  const [isFullscreen, setIsFullscreen] = useState<boolean>(false);
   const windowSize = useWindowSize();
+  const debouncedWindowWidth = useDebouncedValue(windowSize.width);
 
-  useEffect(() => {
-    (async function () {
-      // Fullscreen state isn't updated right after resize event on Mac (needs to wait for animation) so
-      // we'll poll for 10 seconds to see if it changes. Hopefully Tauri eventually adds a way to listen
-      // for this.
-      for (let i = 0; i < 100; i++) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        const newIsFullscreen = await getCurrent().isFullscreen();
-        if (newIsFullscreen !== isFullscreen) {
-          setIsFullscreen(newIsFullscreen);
-          break;
-        }
-      }
-    })();
-  }, [windowSize, isFullscreen]);
+  // NOTE: Fullscreen state isn't updated right after resize event on Mac (needs to wait for animation) so
+  // we'll wait for a bit using the debounced window size. Hopefully Tauri eventually adds a way to listen
+  // for fullscreen change events.
 
-  return isFullscreen;
+  return (
+    useQuery({
+      queryKey: ['is_fullscreen', debouncedWindowWidth],
+      queryFn: async () => {
+        return getCurrent().isFullscreen();
+      },
+    }).data ?? false
+  );
 }
