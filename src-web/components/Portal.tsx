5e2218fd64b59e76ff1ea23ec631dfbb14f458bd
Gregory Schier <gschier1990@gmail.com>
2024-06-04 02:56:52 -0700

    Remove Windows plugin

--- src-web/components/Portal.tsx
+++ src-web/components/Portal.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 import { usePortal } from '../hooks/usePortal';
+
 interface Props {
   children: ReactNode;
   name: string;
