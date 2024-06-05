558b4298077148d530507b15844c91950d592c12
Gregory Schier <gschier1990@gmail.com>
2024-06-05 06:53:38 -0700

    Fix copy-as-curl variables

--- src-web/hooks/useCopyAsCurl.tsx
+++ src-web/hooks/useCopyAsCurl.tsx
@@ -1,10 +1,12 @@
 import { invoke } from '@tauri-apps/api/core';
+import { useActiveEnvironmentId } from './useActiveEnvironmentId';
 import { useClipboardText } from './useClipboardText';
 
 export function useCopyAsCurl(requestId: string) {
   const [, copy] = useClipboardText();
+  const environmentId = useActiveEnvironmentId();
   return async () => {
-    const cmd: string = await invoke('cmd_request_to_curl', { requestId });
+    const cmd: string = await invoke('cmd_request_to_curl', { requestId, environmentId });
     copy(cmd);
     return cmd;
   };
