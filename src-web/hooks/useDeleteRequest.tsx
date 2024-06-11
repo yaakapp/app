26cc467858f931ae9cecbad07a3edaedb2674b4b
Gregory Schier <gschier1990@gmail.com>
2024-06-10 23:17:12 -0700

    Fix delete request in sidebar

--- src-web/hooks/useDeleteRequest.tsx
+++ src-web/hooks/useDeleteRequest.tsx
@@ -1,21 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
-import type { GrpcRequest, HttpRequest } from '../lib/models';
 import { useDeleteAnyGrpcRequest } from './useDeleteAnyGrpcRequest';
 import { useDeleteAnyHttpRequest } from './useDeleteAnyHttpRequest';
 
-export function useDeleteRequest(request: HttpRequest | GrpcRequest | null) {
+export function useDeleteRequest(id: string | null) {
   const deleteAnyHttpRequest = useDeleteAnyHttpRequest();
   const deleteAnyGrpcRequest = useDeleteAnyGrpcRequest();
 
   return useMutation<void, string>({
     mutationFn: async () => {
-      if (request?.model === 'http_request') {
-        await deleteAnyHttpRequest.mutateAsync(request.id);
-      } else if (request?.model === 'grpc_request') {
-        await deleteAnyGrpcRequest.mutateAsync(request.id);
-      } else {
-        // Request is null
-      }
+      if (id == null) return;
+      // We don't know what type it is based on the ID, so just try deleting both
+      deleteAnyHttpRequest.mutate(id);
+      deleteAnyGrpcRequest.mutate(id);
     },
   });
 }
