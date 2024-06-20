bd948f9cd63a48bf6dde119ff8f5d6ecbe55883e
Gregory Schier <gschier1990@gmail.com>
2024-06-20 09:20:04 -0700

    Actually write the file

--- scripts/replace-version.cjs
+++ scripts/replace-version.cjs
@@ -1,9 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-const tauriConfig = fs.readFileSync(path.join(__dirname, '../src-tauri/tauri.conf.json'), 'utf8');
+const configPath = path.join(__dirname, '../src-tauri/tauri.conf.json');
+
+const tauriConfig = fs.readFileSync(configPath, 'utf8');
 if (!process.env.YAAK_VERSION) {
   throw new Error('YAAK_VERSION environment variable not set')
 }
 
-console.log(tauriConfig.replaceAll('__YAAK_VERSION__', process.env.YAAK_VERSION.replace('v', '')));
+const newTauriConfig = tauriConfig.replaceAll('__YAAK_VERSION__', process.env.YAAK_VERSION.replace('v', ''));
+fs.writeFileSync(configPath, newTauriConfig);
