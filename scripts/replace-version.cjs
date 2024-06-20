907836a75190a33b01146c548f0b0030c17ba473
Gregory Schier <gschier1990@gmail.com>
2024-06-20 09:23:50 -0700

    Update script

--- scripts/replace-version.cjs
+++ scripts/replace-version.cjs
@@ -1,12 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
-const configPath = path.join(__dirname, '../src-tauri/tauri.conf.json');
+const tauriConfigPath = path.join(__dirname, '../src-tauri/tauri.conf.json');
 
-const tauriConfig = fs.readFileSync(configPath, 'utf8');
-if (!process.env.YAAK_VERSION) {
+const tauriConfig = fs.readFileSync(tauriConfigPath, 'utf8');
+const version = process.env.YAAK_VERSION?.replace('v', '');
+if (!version) {
   throw new Error('YAAK_VERSION environment variable not set')
 }
 
-const newTauriConfig = tauriConfig.replaceAll('__YAAK_VERSION__', process.env.YAAK_VERSION.replace('v', ''));
-fs.writeFileSync(configPath, newTauriConfig);
+console.log('Writing version ' + version + ' to ' + tauriConfigPath)
+const newTauriConfig = tauriConfig.replaceAll('__YAAK_VERSION__', version);
+fs.writeFileSync(tauriConfigPath, newTauriConfig);
