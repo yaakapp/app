e8bcc695bb132cba5b235d2015433a0d614767b8
Gregory Schier <gschier1990@gmail.com>
2024-06-20 09:03:24 -0700

    Better fallback request name generation

--- src-web/lib/fallbackRequestName.ts
+++ src-web/lib/fallbackRequestName.ts
@@ -3,31 +3,25 @@ import type { GrpcRequest, HttpRequest } from './models';
 export function fallbackRequestName(r: HttpRequest | GrpcRequest | null): string {
   if (r == null) return '';
 
+  // Return name if it has one
   if (r.name) {
     return r.name;
   }
 
-  const withoutVariables = r.url.replace(/\$\{\[\s*([^\]]+)\s*]}/g, '$1');
+  // Replace variable syntax with variable name
+  const withoutVariables = r.url.replace(/\$\{\[\s*([^\]\s]+)\s*]}/g, '$1');
   if (withoutVariables.trim() === '') {
     return r.model === 'http_request' ? 'New HTTP Request' : 'new gRPC Request';
   }
 
-  const fixedUrl = withoutVariables.match(/^https?:\/\//)
-    ? withoutVariables
-    : 'http://' + withoutVariables;
-
+  // GRPC gets nice short names
   if (r.model === 'grpc_request' && r.service != null && r.method != null) {
     const shortService = r.service.split('.').pop();
     return `${shortService}/${r.method}`;
-  } else {
-    try {
-      const url = new URL(fixedUrl);
-      const pathname = url.pathname === '/' ? '' : url.pathname;
-      return `${url.host}${pathname}`;
-    } catch (_) {
-      // Nothing
-    }
   }
 
-  return r.url;
+  // Strip unnecessary protocol
+  const withoutProto = withoutVariables.replace(/^https?:\/\//, '');
+
+  return withoutProto;
 }
