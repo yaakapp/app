c7900b3a7c47f06ccafb268e4ef25e6676da297c
Gregory Schier <gschier1990@gmail.com>
2024-06-21 11:39:54 -0700

    Basic gRPC server to run Curl import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,7 @@ module.exports = {
   parserOptions: {
     project: ["./tsconfig.json"]
   },
-  ignorePatterns: ["scripts/**/*", "src-tauri/**/*", "plugins/**/*"],
+  ignorePatterns: ["scripts/**/*", "plugin-runtime/**/*", "src-tauri/**/*", "plugins/**/*"],
   settings: {
     react: {
       version: "detect"
---
+++ plugin-runtime/plugins/importer-curl/index.js
@@ -0,0 +1,3 @@
+module.exports = function main(name) {
+  return `Hello ${name}`;
+};
