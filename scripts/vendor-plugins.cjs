5abf460fcef8ab32e9841a97612447feaa3175ac
Gregory Schier <gschier1990@gmail.com>
2024-07-21 22:18:45 -0700

    Some small refactoring

--- scripts/vendor-plugins.cjs
+++ scripts/vendor-plugins.cjs
@@ -1,20 +1,20 @@
 const {readdirSync, cpSync} = require("node:fs");
 const path = require("node:path");
 const {execSync} = require("node:child_process");
-const PLUGINS_DIR = process.env.YAAK_PLUGINS_DIR;
-if (!PLUGINS_DIR) {
+const pluginsDir = process.env.YAAK_PLUGINS_DIR;
+if (!pluginsDir) {
   console.log("YAAK_PLUGINS_DIR is not set");
   process.exit(1);
 }
 
-console.log('Installing Yaak plugins dependencies', PLUGINS_DIR);
-execSync('npm ci', {cwd: PLUGINS_DIR});
-console.log('Building Yaak plugins', PLUGINS_DIR);
-execSync('npm run build', {cwd: PLUGINS_DIR});
+console.log('Installing Yaak plugins dependencies', pluginsDir);
+execSync('npm ci', {cwd: pluginsDir});
+console.log('Building Yaak plugins', pluginsDir);
+execSync('npm run build', {cwd: pluginsDir});
 
-console.log('Copying Yaak plugins to', PLUGINS_DIR);
+console.log('Copying Yaak plugins to', pluginsDir);
 
-const pluginsRoot = path.join(PLUGINS_DIR, 'plugins');
+const pluginsRoot = path.join(pluginsDir, 'plugins');
 for (const name of readdirSync(pluginsRoot)) {
   const dir = path.join(pluginsRoot, name);
   if (name.startsWith('.')) continue;
