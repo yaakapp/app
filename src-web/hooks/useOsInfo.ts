e461851f6f84b9694ed627d8ac5178879435ec2b
Gregory Schier <gschier1990@gmail.com>
2024-08-09 15:35:21 -0700

    Append [DEV] to window title in dev

--- src-web/hooks/useOsInfo.ts
+++ src-web/hooks/useOsInfo.ts
@@ -1,12 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
-import type { OsType } from '@tauri-apps/plugin-os';
 import { type } from '@tauri-apps/plugin-os';
 
 export function useOsInfo() {
-  return useQuery<{ osType: OsType }>({
-    queryKey: ['platform'],
-    queryFn: async () => {
-      return { osType: await type() };
-    },
-  }).data;
+  return { osType: type() };
 }
