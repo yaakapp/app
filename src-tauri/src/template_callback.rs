55122b042bdee046e0da4f4ec4c0e992a8abea02
Gregory Schier <gschier1990@gmail.com>
2024-08-26 13:10:22 -0700

    Default template fn args

--- src-tauri/src/template_callback.rs
+++ src-tauri/src/template_callback.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use tauri::{AppHandle, Manager};
-use yaak_plugin_runtime::events::RenderPurpose;
+use yaak_plugin_runtime::events::{RenderPurpose, TemplateFunctionArg};
 use yaak_plugin_runtime::manager::PluginManager;
 use yaak_templates::TemplateCallback;
 
@@ -12,7 +12,10 @@ pub struct PluginTemplateCallback {
 
 impl PluginTemplateCallback {
     pub fn new(app_handle: AppHandle) -> PluginTemplateCallback {
-        PluginTemplateCallback { app_handle, purpose: RenderPurpose::Preview }
+        PluginTemplateCallback {
+            app_handle,
+            purpose: RenderPurpose::Preview,
+        }
     }
 
     pub fn for_send(&self) -> PluginTemplateCallback {
@@ -25,8 +28,32 @@ impl PluginTemplateCallback {
 impl TemplateCallback for PluginTemplateCallback {
     async fn run(&self, fn_name: &str, args: HashMap<String, String>) -> Result<String, String> {
         let plugin_manager = self.app_handle.state::<PluginManager>();
+        let function = plugin_manager
+            .get_template_functions()
+            .await
+            .map_err(|e| e.to_string())?
+            .iter()
+            .flat_map(|f| f.functions.clone())
+            .find(|f| f.name == fn_name)
+            .ok_or("")?;
+
+        let mut args_with_defaults = args.clone();
+        
+        // Fill in default values for all args
+        for a_def in function.args {
+            let base = match a_def {
+                TemplateFunctionArg::Text(a) => a.base,
+                TemplateFunctionArg::Select(a) => a.base,
+                TemplateFunctionArg::Checkbox(a) => a.base,
+                TemplateFunctionArg::HttpRequest(a) => a.base,
+            };
+            if let None = args_with_defaults.get(base.name.as_str()) {
+                args_with_defaults.insert(base.name, base.default_value.unwrap_or_default());
+            }
+        }
+
         let resp = plugin_manager
-            .call_template_function(fn_name, args, self.purpose.clone())
+            .call_template_function(fn_name, args_with_defaults, self.purpose.clone())
             .await
             .map_err(|e| e.to_string())?;
         Ok(resp.unwrap_or_default())
