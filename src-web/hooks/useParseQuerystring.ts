0f05e8925e718849dea9ea265e7af0ff29a5a22d
Gregory Schier <gschier1990@gmail.com>
2024-09-15 11:09:36 -0700

    Import query params from URL

---
+++ src-web/hooks/useParseQuerystring.ts
@@ -0,0 +1,60 @@
+import { useMutation } from '@tanstack/react-query';
+import type { HttpUrlParameter } from '@yaakapp/api';
+import { useToast } from '../components/ToastContext';
+import { getHttpRequest } from '../lib/store';
+import { useRequestEditor } from './useRequestEditor';
+import { useUpdateAnyHttpRequest } from './useUpdateAnyHttpRequest';
+
+export function useParseQuerystring(requestId: string) {
+  const updateRequest = useUpdateAnyHttpRequest();
+  const toast = useToast();
+  const [, { focusParamsTab, forceParamsRefresh, forceUrlRefresh }] = useRequestEditor();
+
+  return useMutation({
+    mutationKey: ['parse_query_string'],
+    mutationFn: async (url: string) => {
+      const [baseUrl, querystring] = url.split('?') || null;
+      if (querystring == null) return;
+
+      const request = await getHttpRequest(requestId);
+      if (request == null) return;
+
+      const parsedParams = Array.from(new URLSearchParams(querystring).entries());
+      const additionalUrlParameters: HttpUrlParameter[] = parsedParams.map(
+        ([name, value]): HttpUrlParameter => ({
+          name,
+          value,
+          enabled: true,
+        }),
+      );
+
+      const urlParameters: HttpUrlParameter[] = [...request.urlParameters];
+      for (const newParam of additionalUrlParameters) {
+        const index = urlParameters.findIndex((p) => p.name === newParam.name);
+        if (index >= 0) {
+          urlParameters[index]!.value = newParam.value;
+        } else {
+          urlParameters.push(newParam);
+        }
+      }
+
+      await updateRequest.mutateAsync({
+        id: requestId,
+        update: {
+          url: baseUrl ?? request.url,
+          urlParameters,
+        },
+      });
+
+      toast.show({
+        id: 'querystring-imported',
+        variant: 'info',
+        message: 'Imported query params from URL',
+      });
+
+      focusParamsTab();
+      forceUrlRefresh();
+      forceParamsRefresh();
+    },
+  });
+}
