f201857d515f78bc99a2c5ffd49f07f8ea1bcca0
Gregory Schier <gschier1990@gmail.com>
2024-10-12 20:57:01 -0700

    Bump Tauri to fix settings window

--- src-web/components/App.tsx
+++ src-web/components/App.tsx
@@ -24,7 +24,7 @@ const queryClient = new QueryClient({
   },
 });
 
-const ENABLE_REACT_QUERY_DEVTOOLS = true;
+const ENABLE_REACT_QUERY_DEVTOOLS = false;
 
 export function App() {
   return (
--- src-web/hooks/useOpenSettings.tsx
+++ src-web/hooks/useOpenSettings.tsx
@@ -1,30 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
-import { useDialog } from '../components/DialogContext';
-import Settings from '../components/Settings/Settings';
 import { invokeCmd } from '../lib/tauri';
 import { useActiveWorkspace } from './useActiveWorkspace';
 import { useAppRoutes } from './useAppRoutes';
-import { useOsInfo } from './useOsInfo';
 
 export function useOpenSettings() {
   const routes = useAppRoutes();
   const workspace = useActiveWorkspace();
-  const dialog = useDialog();
-  const { osType } = useOsInfo();
   return useMutation({
     mutationKey: ['open_settings'],
     mutationFn: async () => {
       if (workspace == null) return;
-
-      // HACK: Show settings in dialog on Linux until this is fixed: https://github.com/tauri-apps/tauri/issues/11171
-      if (osType === 'linux') {
-        dialog.show({
-          id: 'settings',
-          size: 'lg',
-          render: ({ hide }) => <Settings hide={hide} />,
-        });
-        return;
-      }
 
       await invokeCmd('cmd_new_child_window', {
         url: routes.paths.workspaceSettings({ workspaceId: workspace.id }),
